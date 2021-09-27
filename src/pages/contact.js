1a8eaa6c3787f8a980d51a7c4fa314693ed6492f
FKMLJF <vincze.jozsef.develop@gmail.com>
2021-09-27 19:58:46 +0200

    generate pages

---
+++ src/pages/contact.js
@@ -0,0 +1,7 @@
+import * as React from "react"
+
+const ContactPage = () => {
+    return (<h1>Kapcsolat</h1>)
+}
+
+export default ContactPage;
---
+++ src/pages/language-certificate.js
@@ -0,0 +1,7 @@
+import * as React from "react"
+
+const LanguageCertificatePage = () => {
+    return (<h1>Nyelvvizsga</h1>)
+}
+
+export default LanguageCertificatePage
